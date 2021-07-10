fe451426cf21c9771c77da1eb29eb3a7ae70a096
Tom Käppler <tomkaeppler@web.de>
2021-07-10 23:37:02 +0200

    fix using of any type

--- src/graphql/context.ts
+++ src/graphql/context.ts
@@ -1,4 +1,6 @@
-export function context({ req }) {
+import type { NextApiRequest } from 'next';
+
+export function context({ req }: { req: NextApiRequest }) {
   // Get the user token from the header
   const token = req.headers.authorization || '';
 
