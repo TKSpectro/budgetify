ff54dc4d87ebfc21deb1d8c1935d24401c48b580
Tom Käppler <tomkaeppler@web.de>
2021-07-21 18:38:11 +0200

    add types for context

--- src/graphql/context.ts
+++ src/graphql/context.ts
@@ -1,18 +1,45 @@
 import { AuthenticationError } from 'apollo-server-micro';
 import jwt from 'jsonwebtoken';
-import type { NextApiRequest } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-export function context({ req }: { req: NextApiRequest }) {
-  const token = req.cookies.authToken || null;
+export interface ContextUser {
+  id: string;
+  email: string;
+}
+
+export interface ContextInput {
+  req: NextApiRequest;
+  res: NextApiResponse;
+}
+
+export interface Context {
+  req: NextApiRequest;
+  res: NextApiResponse;
+  user: ContextUser;
+}
+
+export function context({ req, res }: ContextInput): Context {
+  const token = req.cookies.authToken || undefined;
 
   if (!process.env.JWT_SECRET) {
     throw new AuthenticationError('Server is not setup correctly');
   }
 
-  let user: string | jwt.JwtPayload = {};
-  if (token) {
-    user = jwt.verify(token, process.env.JWT_SECRET);
+  let user: ContextUser = { id: '', email: '' };
+
+  try {
+    if (token) {
+      const data = jwt.verify(token, process.env.JWT_SECRET);
+
+      if (typeof data !== 'string') {
+        user.id = data.id;
+        user.email = data.email;
+      }
+    }
+  } catch (error) {
+    // TODO: handle error?
+    throw Error('Context: Token was not valid');
   }
 
-  return { user };
+  return { req, res, user } as Context;
 }
