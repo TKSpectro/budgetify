b1fbdd48c6328a4af5ab2b7aaec920f4e9e61181
Tom Käppler <tomkaeppler@web.de>
2021-08-20 14:06:24 +0200

    setup next security headers again

--- next.config.js
+++ next.config.js
@@ -2,24 +2,31 @@
 const ContentSecurityPolicyData = `
   default-src 'self';
   script-src 'self' 'unsafe-eval' 'unsafe-inline';
-  child-src 'none';
-  style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src 'none';
+  style-src 'self' 'unsafe-inline';
+  child-src 'none';
   connect-src *;
   font-src 'self';
 `;
 
+// https://nextjs.org/docs/advanced-features/security-headers
 const securityHeaders = [
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
   {
     key: 'Content-Security-Policy',
+    // Need to replace line breaks so its a one line string
     value: ContentSecurityPolicyData.replace(/\n/g, ''),
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
-  {
-    key: 'Referrer-Policy',
-    value: 'origin-when-cross-origin',
   },
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
   {
@@ -31,17 +38,32 @@ const securityHeaders = [
     key: 'X-Content-Type-Options',
     value: 'nosniff',
   },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+  {
+    key: 'Permissions-Policy',
+    value: 'origin-when-cross-origin',
+  },
 ];
 
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
+module.exports = {
   webpack: (config) => {
     config.experiments = { topLevelAwait: true };
+    //config.optimization.minimize = false;
     return config;
   },
-  //securityHeaders,
+  headers: () => [
+    {
+      // Apply these headers to all routes in your application.
+      source: '/(.*)',
+      headers: securityHeaders,
+    },
+  ],
 };
-
-module.exports = nextConfig;
