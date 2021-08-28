68ed1e132c1d989d21b222ba579b6f349347a1e8
Tom Käppler <tomkaeppler@web.de>
2021-08-28 17:08:30 +0200

    add useInvite to household choosing page

--- src/pages/households/index.tsx
+++ src/pages/households/index.tsx
@@ -1,12 +1,16 @@
-import { gql, useQuery } from '@apollo/client';
+import { gql, useMutation, useQuery } from '@apollo/client';
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
+import { useForm } from 'react-hook-form';
 import { Alert } from '~/components/UI/Alert';
 import { Container } from '~/components/UI/Container';
 import { Error } from '~/components/UI/Error';
+import { Form } from '~/components/UI/Form';
+import { Input } from '~/components/UI/Input';
 import { LoadingAnimation } from '~/components/UI/LoadingAnimation';
-import { Household } from '~/graphql/__generated__/types';
+import { Modal } from '~/components/UI/Modal';
+import { Household, MutationUseInviteArgs } from '~/graphql/__generated__/types';
 import { preloadQuery } from '~/utils/apollo';
 import { authenticatedRoute } from '~/utils/auth';
 
@@ -23,27 +27,64 @@ const HOUSEHOLD_LIST_QUERY = gql`
   }
 `;
 
+const USE_INVITE_TOKEN_MUTATION = gql`
+  mutation USE_INVITE_TOKEN_MUTATION($token: String!) {
+    useInvite(token: $token) {
+      id
+    }
+  }
+`;
+
 export default function Households() {
-  const { data, loading, error } = useQuery(HOUSEHOLD_LIST_QUERY);
-  if (loading) return <span>loading...</span>;
+  const { data, loading, error, refetch } = useQuery(HOUSEHOLD_LIST_QUERY);
+  const form = useForm<MutationUseInviteArgs>({ defaultValues: { token: '' } });
+
+  const [UseInviteMutation, { error: useInviteError }] = useMutation(USE_INVITE_TOKEN_MUTATION, {
+    onCompleted: () => {
+      refetch();
+      form.reset();
+    },
+    onError: () => {
+      form.reset();
+    },
+  });
+
+  const onSubmitHandler = () => {
+    UseInviteMutation({ variables: { token: form.getValues('token') } });
+  };
 
   const households = data?.households || [];
 
   // TODO: What info should be shown for each household?
   // Maybe even the net-value of the household?
   // Add a open button?
-
   return (
     <>
       <Head>
-        <title>Dashboard | budgetify</title>
+        <title>Households | budgetify</title>
       </Head>
       <Container>
-        <Error title="Failed to load recurring messages" error={error} />
+        <Error title="Failed to load households" error={error} />
+        <Error title="Failed to use invite token" error={useInviteError} />
         {loading && <LoadingAnimation />}
         {!loading && !error && households.length === 0 ? (
           <Alert message="Could not find any households. Please create or join one." type="error" />
         ) : null}
+
+        <div className="mb-4">
+          <Modal
+            buttonText="Use Invite Token"
+            title="Use a Invite Token"
+            description=""
+            onSubmit={onSubmitHandler}
+            submitText="Use Token"
+          >
+            <Form onSubmit={() => {}} form={form}>
+              <Input label="Token" {...form.register('token')}></Input>
+            </Form>
+          </Modal>
+        </div>
+
         {households?.map((household: Household) => {
           return (
             <Link href={`/households/${household.id}`} passHref key={household.id}>
@@ -60,6 +101,7 @@ export default function Households() {
     </>
   );
 }
+
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   authenticatedRoute(ctx);
   return preloadQuery(ctx, {
