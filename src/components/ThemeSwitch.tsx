2a8185594b160708894d848a596e85aa1ed6c059
Tom Käppler <t.kaeppler@zebresel.com>
2021-09-07 11:54:54 +0200

    fix todo themeswitcher being buggy on first page open. was caused because default setting was set to system but i checked for "dark" and not !"light"

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -10,8 +10,6 @@ export function ThemeSwitch() {
     setTheme(theme === 'light' ? 'dark' : 'light');
   }
 
-  // TODO: Looks buggy when page gets opened for the first time on a new device
-
   // Clean and good looking implementation of a switch (link is for vue -> small changes)
   // https://medium.com/front-end-weekly/build-a-html-toggle-switch-in-just-7-lines-of-code-using-vue-tailwindcss-ed215394fcd
   // Also add icons depending on the state
@@ -21,13 +19,13 @@ export function ThemeSwitch() {
         <div
           className={clsx(
             'w-16 h-10 items-center bg-gray-300 rounded-full p-1 duration-300 ease-in-out',
-            { 'bg-brand-400': theme === 'dark' },
+            { 'bg-brand-400': theme !== 'light' },
           )}
         >
           <div
             className={clsx(
               'bg-white dark:bg-gray-800 w-8 h-8 rounded-full shadow-md transform duration-300 ease-in-out',
-              { 'translate-x-6': theme === 'dark' },
+              { 'translate-x-6': theme! == 'light' },
             )}
           >
             {theme === 'light' ? (
