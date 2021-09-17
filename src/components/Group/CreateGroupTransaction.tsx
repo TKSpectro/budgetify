ebdea967e71df24f747d0061b93d7e2257e94120
Tom Kaeppler <tomkaeppler@web.de>
2021-09-17 13:43:05 +0200

    refactor createGroupTransaction form into a new custom component.

---
+++ src/components/Group/CreateGroupTransaction.tsx
@@ -0,0 +1,186 @@
+import { gql, useMutation } from '@apollo/client';
+import { CheckIcon, MinusIcon, PlusIcon, XIcon } from '@heroicons/react/outline';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import {
+  MutationCreateGroupTransactionArgs,
+  TransactionType,
+  User,
+} from '~/graphql/__generated__/types';
+import { Error } from '../UI/Error';
+import { Input } from '../UI/Input';
+import { ModalForm } from '../UI/ModalForm';
+import { Switch } from '../UI/Switch';
+import { UserMultiSelect } from './UserMultiselect';
+
+const CREATE_GROUP_TRANSACTION_MUTATION = gql`
+  mutation CREATE_GROUP_TRANSACTION_MUTATION(
+    $name: String!
+    $value: Money!
+    $type: TransactionType!
+    $groupId: String!
+    $participantIds: [String!]!
+  ) {
+    createGroupTransaction(
+      name: $name
+      value: $value
+      type: $type
+      groupId: $groupId
+      participantIds: $participantIds
+    ) {
+      id
+    }
+  }
+`;
+
+interface Props {
+  members: User[];
+}
+
+export function CreateGroupTransaction({ members }: Props) {
+  const router = useRouter();
+  const groupId = router.query.groupId as string;
+
+  const [
+    createGroupTransaction,
+    {
+      data: createGroupTransactionData,
+      loading: createGroupTransactionLoading,
+      error: createGroupTransactionError,
+    },
+  ] = useMutation(CREATE_GROUP_TRANSACTION_MUTATION, {
+    onCompleted: () => {},
+    onError: () => {},
+    refetchQueries: ['GROUP_QUERY'],
+  });
+
+  const formCreateGroupTransaction = useForm<MutationCreateGroupTransactionArgs>({
+    defaultValues: { name: '', value: 0, groupId: groupId as string, participantIds: [] },
+  });
+
+  const onSubmitHandler = () => {
+    // if the user is topping up his account we overwrite the participantIds with an empty array
+    // so its more consistent on the database layer
+    if (formStateIsCashout) {
+      createGroupTransaction({
+        variables: {
+          ...formCreateGroupTransaction.getValues(),
+          // Force the value to be negative as the user input will be positive
+          value: -Math.abs(formCreateGroupTransaction.getValues('value')),
+          type: formStateIsBuyingFood ? TransactionType.Buy : TransactionType.TakeOut,
+          // Check if the all user switch is set depending on that we take all group members
+          // or just the ones that are checked.
+          participantIds: formAllGroupMembers
+            ? members.map((member) => member.id)
+            : formCreateGroupTransaction.getValues('participantIds'),
+        },
+      });
+    } else {
+      // Top up
+      createGroupTransaction({
+        variables: {
+          ...formCreateGroupTransaction.getValues(),
+          type: TransactionType.TopUp,
+          participantIds: [],
+        },
+      });
+    }
+  };
+
+  // Need to handle the participantIds with a custom function which will get called from the UserPicker
+  const setValueHandlerParticipantIds = (members: User[]) => {
+    formCreateGroupTransaction.setValue(
+      'participantIds',
+      members.map((member) => member.id),
+    );
+  };
+
+  const [formStateIsCashout, setFormStateIsCashout] = useState(false);
+  const [formStateIsBuyingFood, setFormStateIsBuyingFood] = useState(true);
+  const [formAllGroupMembers, setFormAllGroupMembers] = useState(true);
+  return (
+    <>
+      <Error title="Could not create transaction." error={createGroupTransactionError} />
+      <ModalForm
+        title="New Transaction"
+        buttonText="New Transaction"
+        buttonClassName="md:absolute right-4 top-2 text-base"
+        form={formCreateGroupTransaction}
+        submitText="Create"
+        description={`You can switch between topping up your account (account balance) and buying food / taking money out of the group balance`}
+        onSubmit={onSubmitHandler}
+      >
+        {/* // TODO: Somehow make this look and feel better */}
+        <label>
+          Switch between transaction modes
+          <Switch
+            isLeft={formStateIsCashout}
+            onClick={() => setFormStateIsCashout(!formStateIsCashout)}
+            leftIcon={<PlusIcon className="w-4 h-4 " />}
+            rightIcon={<MinusIcon className="w-4 h-4 text-brand-400" />}
+          />
+        </label>
+
+        <Input
+          label="Name*"
+          type="text"
+          {...formCreateGroupTransaction.register('name', {
+            required: { value: true, message: 'Name is required' },
+            minLength: { value: 3, message: 'Name must be at least 3 characters' },
+          })}
+        />
+
+        {formStateIsCashout && (
+          <label>
+            Take out money / Bought food
+            <Switch
+              isLeft={formStateIsBuyingFood}
+              onClick={() => setFormStateIsBuyingFood(!formStateIsBuyingFood)}
+              leftIcon={<XIcon className="w-4 h-4 " />}
+              rightIcon={<CheckIcon className="w-4 h-4 text-brand-400" />}
+            />
+          </label>
+        )}
+
+        <Input
+          label={
+            formStateIsCashout
+              ? formStateIsBuyingFood
+                ? 'Bought food*'
+                : 'Take money out*'
+              : 'Top up account balance*'
+          }
+          type="number"
+          step="any"
+          {...formCreateGroupTransaction.register('value', {
+            required: { value: true, message: 'Value is required' },
+            min: { value: 0, message: 'Value must be positive' },
+            valueAsNumber: true,
+          })}
+        />
+
+        {/* // TODO: Somehow make this look and feel better */}
+        {formStateIsCashout && formStateIsBuyingFood && (
+          <label>
+            All group members?
+            <Switch
+              isLeft={formAllGroupMembers}
+              onClick={() => setFormAllGroupMembers(!formAllGroupMembers)}
+              leftIcon={<XIcon className="w-4 h-4 " />}
+              rightIcon={<CheckIcon className="w-4 h-4 text-brand-400" />}
+            />
+          </label>
+        )}
+
+        {formStateIsCashout && !formAllGroupMembers && (
+          <UserMultiSelect
+            items={members}
+            setValue={setValueHandlerParticipantIds}
+            {...formCreateGroupTransaction.register('participantIds')}
+          />
+        )}
+      </ModalForm>
+    </>
+  );
+}
--- src/pages/groups/[groupId]/index.tsx
+++ src/pages/groups/[groupId]/index.tsx
@@ -1,28 +1,20 @@
-import { gql, useMutation, useQuery } from '@apollo/client';
-import { CheckIcon, MinusIcon, PlusIcon, XIcon } from '@heroicons/react/outline';
+import { gql, useQuery } from '@apollo/client';
 import clsx from 'clsx';
 import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { CreateGroupTransaction } from '~/components/Group/CreateGroupTransaction';
 import { NewThreshold } from '~/components/Group/NewThreshold';
-import { UserMultiSelect } from '~/components/Group/UserMultiselect';
 import { Container } from '~/components/UI/Container';
 import { Disclosure } from '~/components/UI/Disclosure';
 import { Error } from '~/components/UI/Error';
-import { Input } from '~/components/UI/Input';
 import { Link } from '~/components/UI/Link';
 import { Loader } from '~/components/UI/Loader';
-import { ModalForm } from '~/components/UI/ModalForm';
 import { Progressbar } from '~/components/UI/Progressbar';
-import { Switch } from '~/components/UI/Switch';
 import {
   GroupTransaction,
-  MutationCreateGroupTransactionArgs,
   Participant,
   Threshold,
   ThresholdType,
-  TransactionType,
   User,
 } from '~/graphql/__generated__/types';
 import { preloadQuery } from '~/utils/apollo';
@@ -69,86 +61,11 @@ const GROUP_QUERY = gql`
   }
 `;
 
-const CREATE_GROUP_TRANSACTION_MUTATION = gql`
-  mutation CREATE_GROUP_TRANSACTION_MUTATION(
-    $name: String!
-    $value: Money!
-    $type: TransactionType!
-    $groupId: String!
-    $participantIds: [String!]!
-  ) {
-    createGroupTransaction(
-      name: $name
-      value: $value
-      type: $type
-      groupId: $groupId
-      participantIds: $participantIds
-    ) {
-      id
-    }
-  }
-`;
-
 export default function Group() {
   const router = useRouter();
   const groupId = router.query.groupId as string;
 
   const { data, loading, error, refetch } = useQuery(GROUP_QUERY, { variables: { id: groupId } });
-
-  const [
-    createGroupTransaction,
-    {
-      data: createGroupTransactionData,
-      loading: createGroupTransactionLoading,
-      error: createGroupTransactionError,
-    },
-  ] = useMutation(CREATE_GROUP_TRANSACTION_MUTATION, {
-    onCompleted: () => {
-      refetch();
-    },
-    onError: () => {},
-  });
-
-  const formCreateGroupTransaction = useForm<MutationCreateGroupTransactionArgs>({
-    defaultValues: { name: '', value: 0, groupId: groupId as string, participantIds: [] },
-  });
-
-  const onSubmitHandler = () => {
-    // if the user is topping up his account we overwrite the participantIds with an empty array
-    // so its more consistent on the database layer
-    if (formStateIsCashout) {
-      createGroupTransaction({
-        variables: {
-          ...formCreateGroupTransaction.getValues(),
-          // Force the value to be negative as the user input will be positive
-          value: -Math.abs(formCreateGroupTransaction.getValues('value')),
-          type: formStateIsBuyingFood ? TransactionType.Buy : TransactionType.TakeOut,
-          // Check if the all user switch is set depending on that we take all group members
-          // or just the ones that are checked.
-          participantIds: formAllGroupMembers
-            ? members.map((member) => member.id)
-            : formCreateGroupTransaction.getValues('participantIds'),
-        },
-      });
-    } else {
-      // Top up
-      createGroupTransaction({
-        variables: {
-          ...formCreateGroupTransaction.getValues(),
-          type: TransactionType.TopUp,
-          participantIds: [],
-        },
-      });
-    }
-  };
-
-  // Need to handle the participantIds with a custom function which will get called from the UserPicker
-  const setValueHandlerParticipantIds = (members: User[]) => {
-    formCreateGroupTransaction.setValue(
-      'participantIds',
-      members.map((member) => member.id),
-    );
-  };
 
   const group = data?.group;
 
@@ -157,15 +74,11 @@ export default function Group() {
 
   const thresholds = group?.thresholds;
 
-  const [formStateIsCashout, setFormStateIsCashout] = useState(false);
-  const [formStateIsBuyingFood, setFormStateIsBuyingFood] = useState(true);
-  const [formAllGroupMembers, setFormAllGroupMembers] = useState(true);
-
   return (
     <>
       <Container>
         <Error title="Could not load group." error={error} />
-        <Error title="Could not create transaction." error={createGroupTransactionError} />
+
         <Loader loading={loading} />
 
         {group && (
@@ -173,85 +86,8 @@ export default function Group() {
             <div className="text-xl font-bold ">{group.name}</div>
             <div className="text-lg font-medium">Group balance: {group.value}€</div>
 
-            <ModalForm
-              title="New Transaction"
-              buttonText="New Transaction"
-              buttonClassName="md:absolute right-4 top-2 text-base"
-              form={formCreateGroupTransaction}
-              submitText="Create"
-              description={`You can switch between topping up your account (account balance) and buying food / taking money out of the group balance`}
-              onSubmit={onSubmitHandler}
-            >
-              {/* // TODO: Somehow make this look and feel better */}
-              <label>
-                Switch between transaction modes
-                <Switch
-                  isLeft={formStateIsCashout}
-                  onClick={() => setFormStateIsCashout(!formStateIsCashout)}
-                  leftIcon={<PlusIcon className="w-4 h-4 " />}
-                  rightIcon={<MinusIcon className="w-4 h-4 text-brand-400" />}
-                />
-              </label>
+            <CreateGroupTransaction members={members} />
 
-              <Input
-                label="Name*"
-                type="text"
-                {...formCreateGroupTransaction.register('name', {
-                  required: { value: true, message: 'Name is required' },
-                  minLength: { value: 3, message: 'Name must be at least 3 characters' },
-                })}
-              />
-
-              {formStateIsCashout && (
-                <label>
-                  Take out money / Bought food
-                  <Switch
-                    isLeft={formStateIsBuyingFood}
-                    onClick={() => setFormStateIsBuyingFood(!formStateIsBuyingFood)}
-                    leftIcon={<XIcon className="w-4 h-4 " />}
-                    rightIcon={<CheckIcon className="w-4 h-4 text-brand-400" />}
-                  />
-                </label>
-              )}
-
-              <Input
-                label={
-                  formStateIsCashout
-                    ? formStateIsBuyingFood
-                      ? 'Bought food*'
-                      : 'Take money out*'
-                    : 'Top up account balance*'
-                }
-                type="number"
-                step="any"
-                {...formCreateGroupTransaction.register('value', {
-                  required: { value: true, message: 'Value is required' },
-                  min: { value: 0, message: 'Value must be positive' },
-                  valueAsNumber: true,
-                })}
-              />
-
-              {/* // TODO: Somehow make this look and feel better */}
-              {formStateIsCashout && formStateIsBuyingFood && (
-                <label>
-                  All group members?
-                  <Switch
-                    isLeft={formAllGroupMembers}
-                    onClick={() => setFormAllGroupMembers(!formAllGroupMembers)}
-                    leftIcon={<XIcon className="w-4 h-4 " />}
-                    rightIcon={<CheckIcon className="w-4 h-4 text-brand-400" />}
-                  />
-                </label>
-              )}
-
-              {formStateIsCashout && !formAllGroupMembers && (
-                <UserMultiSelect
-                  items={members}
-                  setValue={setValueHandlerParticipantIds}
-                  {...formCreateGroupTransaction.register('participantIds')}
-                />
-              )}
-            </ModalForm>
             {/* // TODO: Style better */}
             {data && !!group?.owners?.find((x: User) => x?.id === data.me.id) && (
               <div className="md:absolute right-48 top-2 text-base">
