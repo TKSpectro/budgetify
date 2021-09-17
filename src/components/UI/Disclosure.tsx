6c9c68977e3daf079fe7a1f54f99f6f5db9bb09c
Tom Kaeppler <tomkaeppler@web.de>
2021-09-17 13:02:25 +0200

    improve disclosure component. make just the top part clickable and not the "opened" part.

--- src/components/UI/Disclosure.tsx
+++ src/components/UI/Disclosure.tsx
@@ -13,19 +13,21 @@ export function Disclosure({ text, overflowText, showOpen, children, ...props }:
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div onClick={() => setIsOpen(!isOpen)}>
-      {text}
-      {overflowText && (
-        <span className="float-right hidden md:inline md:max-w-[16rem] truncate pr-5">
-          {overflowText}
-        </span>
-      )}
-      {showOpen && (
-        <span className="float-right pr-4">
-          <ChevronDownIcon className="w-5 h-5" />
-        </span>
-      )}
+    <>
+      <div className="hover:cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
+        {text}
+        {overflowText && (
+          <span className="float-right hidden md:inline md:max-w-[16rem] truncate pr-5">
+            {overflowText}
+          </span>
+        )}
+        {showOpen && (
+          <span className="float-right pr-4">
+            <ChevronDownIcon className="w-5 h-5" />
+          </span>
+        )}
+      </div>
       <div className={clsx('my-2 mx-4', { hidden: !isOpen || overflowText })}>{children}</div>
-    </div>
+    </>
   );
 }
