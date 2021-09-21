0aabd50103f6b2733fcd5d4e43174b2ef3ec8bb8
Tom Käppler <t.kaeppler@zebresel.com>
2021-09-21 15:32:57 +0200

    implement UI for creating group invites.

---
+++ src/components/Group/Manage/GroupInviteManager.tsx
@@ -0,0 +1,116 @@
+import { gql, useMutation } from '@apollo/client';
+import { useRouter } from 'next/router';
+import { useForm } from 'react-hook-form';
+import { Error } from '~/components/UI/Error';
+import { Input } from '~/components/UI/Input';
+import { Modal } from '~/components/UI/Modal';
+import { ModalForm } from '~/components/UI/ModalForm';
+import { Invite, MutationCreateGroupInviteArgs } from '~/graphql/__generated__/types';
+
+interface Props {
+  invites: Invite[];
+}
+
+const CREATE_INVITE_MUTATION = gql`
+  mutation createGroupInvite($invitedEmail: String!, $groupId: String!) {
+    createGroupInvite(invitedEmail: $invitedEmail, groupId: $groupId) {
+      id
+    }
+  }
+`;
+
+const DELETE_INVITE_MUTATION = gql`
+  mutation DeleteInvite($id: String!) {
+    deleteInvite(id: $id)
+  }
+`;
+
+export function GroupInviteManager({ invites, ...props }: Props) {
+  const router = useRouter();
+  const groupId = router.query.groupId as string;
+
+  const form = useForm<MutationCreateGroupInviteArgs>({
+    defaultValues: { groupId: groupId, invitedEmail: '' },
+  });
+
+  const [deleteInvite, { error: deleteInviteError }] = useMutation(DELETE_INVITE_MUTATION, {
+    onCompleted: () => {},
+    onError: () => {},
+    refetchQueries: ['QUERY'],
+  });
+
+  const [createInvite, { error: createInviteError }] = useMutation(CREATE_INVITE_MUTATION, {
+    onCompleted: () => {},
+    onError: () => {},
+    refetchQueries: ['QUERY'],
+  });
+
+  const removeHandler = (invite: Invite) => {
+    deleteInvite({ variables: { id: invite.id } });
+  };
+
+  const onSubmitHandler = () => {
+    createInvite({ variables: { ...form.getValues() } });
+  };
+
+  return (
+    <>
+      <Error title="Could not remove invite." error={deleteInviteError} />
+      <Error title="Could not create invite." error={createInviteError} />
+      <ModalForm
+        title="New Invite"
+        onSubmit={onSubmitHandler}
+        submitText="Send invite"
+        buttonText="New Invite"
+        form={form}
+      >
+        <Input
+          label="Invited Email"
+          type="email"
+          {...form.register('invitedEmail', {
+            required: { value: true, message: 'Invited Email is required.' },
+          })}
+        />
+      </ModalForm>
+
+      {invites.length !== 0 ? (
+        <table className="w-full">
+          <tbody className="divide-y divide-gray-200 ">
+            {invites.map((invite: Invite) => {
+              return (
+                <tr key={invite.id} className="">
+                  <td className="py-4">
+                    <div className="max-w-xl ml-2 overflow-auto">
+                      <div className="font-bold text-gray-800 dark:text-gray-100">
+                        {invite.invitedEmail}
+                      </div>
+                      <div className="table-cell sm:hidden  font-bold text-gray-800 dark:text-gray-100">
+                        {new Date(invite.validUntil).toDateString()}
+                      </div>
+                    </div>
+                  </td>
+                  <td className="py-4 hidden sm:table-cell">
+                    <div className="max-w-xl overflow-auto">
+                      <div className="ml-2 font-bold text-gray-800 dark:text-gray-100">
+                        {new Date(invite.validUntil).toDateString()}
+                      </div>
+                    </div>
+                  </td>
+                  <td className="py-4 mr-4 float-right">
+                    <Modal
+                      title="Remove user from household"
+                      description={`Are you sure that you want to remove the invite to ${invite.invitedEmail}?`}
+                      onSubmit={() => removeHandler(invite)}
+                      buttonText="Remove"
+                      submitText="Remove Invite"
+                    />
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      ) : null}
+    </>
+  );
+}
--- src/pages/groups/[groupId]/manage.tsx
+++ src/pages/groups/[groupId]/manage.tsx
@@ -1,6 +1,7 @@
 import { gql, useMutation, useQuery } from '@apollo/client';
 import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
+import { GroupInviteManager } from '~/components/Group/Manage/GroupInviteManager';
 import GroupMemberTable from '~/components/Group/Manage/GroupMemberTable';
 import { Container } from '~/components/UI/Container';
 import { Error } from '~/components/UI/Error';
@@ -9,8 +10,8 @@ import { Modal } from '~/components/UI/Modal';
 import { preloadQuery } from '~/utils/apollo';
 import { authenticatedRoute } from '~/utils/auth';
 
-const GROUP_QUERY = gql`
-  query GROUP_QUERY($id: String!) {
+const QUERY = gql`
+  query QUERY($id: String!) {
     me {
       id
     }
@@ -25,6 +26,11 @@ const GROUP_QUERY = gql`
       members {
         id
         name
+      }
+      invites {
+        id
+        invitedEmail
+        validUntil
       }
     }
   }
@@ -42,7 +48,7 @@ export default function ManageGroup() {
   const router = useRouter();
   const groupId = router.query.groupId as string;
 
-  const { data, loading, error } = useQuery(GROUP_QUERY, { variables: { id: groupId } });
+  const { data, loading, error } = useQuery(QUERY, { variables: { id: groupId } });
 
   const [deleteGroup, { error: deleteGroupError }] = useMutation(DELETE_GROUP_MUTATION, {
     onCompleted: () => {
@@ -57,26 +63,37 @@ export default function ManageGroup() {
 
   const members = group?.members;
   const owners = group?.owners;
+  const invites = group?.invites;
 
   const deleteGroupHandler = () => {
     deleteGroup({ variables: { id: groupId } });
   };
 
   return (
-    <Container>
-      <Error title="Could not load group." error={error} />
-      <Error title="Could not delete group." error={deleteGroupError} />
-      <Loader loading={loading} />
+    <>
+      <Container>
+        <Error title="Could not load group." error={error} />
+        <Error title="Could not delete group." error={deleteGroupError} />
+        <Loader loading={loading} />
 
-      {data && <GroupMemberTable members={members} owners={owners} currentUserId={currentUserId} />}
-      <Modal
-        buttonText="Delete Group"
-        buttonClassName="bg-red-500 mt-4"
-        description="Are you sure that you want to delete this group"
-        onSubmit={deleteGroupHandler}
-        title="Delete Group"
-      />
-    </Container>
+        {data && (
+          <GroupMemberTable members={members} owners={owners} currentUserId={currentUserId} />
+        )}
+        <Modal
+          buttonText="Delete Group"
+          buttonClassName="bg-red-500 mt-4"
+          description="Are you sure that you want to delete this group"
+          onSubmit={deleteGroupHandler}
+          title="Delete Group"
+        />
+      </Container>
+
+      <Container>
+        <Loader loading={loading} />
+        <GroupInviteManager invites={invites} />
+        <Error title="No pending invites." error={invites.length === 0 ? '' : undefined} />
+      </Container>
+    </>
   );
 }
 
@@ -85,7 +102,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     CHECK_GROUP_OWNER: ctx.query.groupId as string,
   });
   return preloadQuery(ctx, {
-    query: GROUP_QUERY,
+    query: QUERY,
     variables: {
       id: ctx.params!.groupId,
     },
