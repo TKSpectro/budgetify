ce2931c1b8049f5956355db4021a87a773adfe04
Tom Käppler <t.kaeppler@zebresel.com>
2021-09-22 15:55:50 +0200

    build managed versions for Modal and ModalForm. These can be use when the state needs to be completely managed by the parent component.

---
+++ src/components/UI/ManagedModal.tsx
@@ -0,0 +1,72 @@
+import { ReactNode } from 'react';
+import { Button, Props as ButtonProps } from './Button';
+
+export interface Props extends ButtonProps {
+  title: string;
+  description?: string;
+  buttonText: string | React.ReactNode;
+  buttonClassName?: string;
+  submitText?: string | React.ReactNode;
+  children?: ReactNode;
+  onSubmit: () => void;
+  showModal: boolean;
+  setShowModal: (value: boolean) => void;
+}
+
+export function ManagedModal({
+  title,
+  description,
+  buttonText,
+  buttonClassName,
+  submitText = buttonText,
+  children,
+  variant,
+  onSubmit,
+  showModal,
+  setShowModal,
+}: Props) {
+  return (
+    <>
+      {showModal ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+            // onClick={() => setShowModal(false)}
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-sm">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white dark:bg-gray-900 outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">{title}</h3>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  {description && (
+                    <p className="my-4 text-blueGray-500 text-lg leading-relaxed">{description}</p>
+                  )}
+                  {children}
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  <Button variant="transparent" onClick={() => setShowModal(false)}>
+                    Close
+                  </Button>
+                  <Button
+                    onClick={() => {
+                      setShowModal(false);
+                      onSubmit();
+                    }}
+                  >
+                    {submitText}
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  );
+}
---
+++ src/components/UI/ManagedModalForm.tsx
@@ -0,0 +1,85 @@
+import { ReactNode } from 'react';
+import { UseFormReturn } from 'react-hook-form';
+import { Button } from './Button';
+import { Form } from './Form';
+
+export interface Props<T> {
+  title: string;
+  description?: string;
+  buttonText: string | React.ReactNode;
+  buttonClassName?: string;
+  submitText?: string | React.ReactNode;
+  children: ReactNode;
+  form: UseFormReturn<T>;
+  onSubmit: () => void;
+  onClick?: () => void;
+  showModal: boolean;
+  setShowModal: (value: boolean) => void;
+}
+
+export function ManagedModalForm<T>({
+  title,
+  description,
+  buttonText,
+  buttonClassName,
+  submitText = buttonText,
+  children,
+  form,
+  onSubmit,
+  onClick = () => {},
+  showModal,
+  setShowModal,
+}: Props<T>) {
+  const onSubmitHandler = () => {
+    onSubmit();
+    form.reset();
+    setShowModal(false);
+  };
+
+  const onCloseHandler = () => {
+    form.reset();
+    setShowModal(false);
+  };
+
+  return (
+    <>
+      {showModal ? (
+        <>
+          <div className="text-left justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-sm md:max-w-lg">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white dark:bg-gray-900 outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">{title}</h3>
+                </div>
+                {/*body*/}
+                {description && (
+                  <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                    <h3 className="">{description}</h3>
+                  </div>
+                )}
+                <Form form={form} onSubmit={onSubmitHandler}>
+                  <div className="relative p-6 flex-auto">{children}</div>
+                  {/*footer*/}
+                  <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                    <Button
+                      type="reset"
+                      className="mr-4"
+                      variant="secondary"
+                      onClick={onCloseHandler}
+                    >
+                      Close
+                    </Button>
+                    <Button type="submit">{submitText}</Button>
+                  </div>
+                </Form>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  );
+}
