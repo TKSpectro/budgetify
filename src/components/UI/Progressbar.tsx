3858d9bb9a99d3f0066609a4a91e56b21f1dfcd8
Tom Käppler <tomkaeppler@web.de>
2021-09-24 12:22:46 +0200

    work on dark styling for progressbar.

--- src/components/UI/Progressbar.tsx
+++ src/components/UI/Progressbar.tsx
@@ -11,29 +11,31 @@ export function Progressbar({ text, type, progress = 0, value, ...props }: Props
   return (
     <div className="relative pt-1">
       <div className="flex mb-2 items-center justify-between">
-        <div>
-          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-brand-600 bg-brand-200">
+        <div className="text-xs font-semibold uppercase text-brand-600 dark:text-brand-200">
+          <span className="inline-block py-1 px-2 rounded-full bg-brand-200 dark:bg-brand-700">
             {text}
           </span>
-          <span className="text-xs font-semibold inline-block py-1 px-2 ml-2 uppercase rounded-full text-brand-600 bg-brand-200">
+          <span className="inline-block py-1 px-2 ml-2 rounded-full bg-brand-200 dark:bg-brand-700">
             {type}
           </span>
           {value && (
-            <span className="text-xs font-semibold inline-block py-1 px-2 ml-2 uppercase rounded-full text-brand-600 bg-brand-200">
+            <span className="inline-block py-1 px-2 ml-2 rounded-full bg-brand-200 dark:bg-brand-700">
               {value}
             </span>
           )}
         </div>
         <div className="text-right">
-          <span className="text-xs font-semibold inline-block text-brand-600">{progress}%</span>
+          <span className="text-xs font-semibold inline-block text-brand-600 dark:text-brand-300">
+            {progress}%
+          </span>
         </div>
       </div>
-      <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-brand-200">
+      <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-brand-200 dark:bg-brand-700">
         <div
           // Need to set width per style because if we would set it in className the tailwindJIT
           // Compiler would not know that he need to generate w-progress at the moment of creation
           style={{ width: progress.toString() + '%' }}
-          className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-brand-500"
+          className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-brand-500 dark:bg-brand-400"
         ></div>
       </div>
     </div>
