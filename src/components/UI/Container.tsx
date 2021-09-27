21abb323c55f307ad61a5cc3960f7cc2808e3434
Tom Käppler <tomkaeppler@web.de>
2021-09-27 11:32:54 +0200

    add a big property to container.

--- src/components/UI/Container.tsx
+++ src/components/UI/Container.tsx
@@ -1,12 +1,21 @@
+import clsx from 'clsx';
 import { ReactNode } from 'react';
 
 interface Props {
   children: ReactNode;
+  big?: boolean;
 }
 
-export function Container({ children }: Props) {
+export function Container({ children, big = false }: Props) {
   return (
-    <div className="relative w-full mx-auto sm:mt-8 sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-3xl 2xl:max-w-4xl sm:rounded-xl shadow-lg bg-white dark:bg-gray-800 p-6">
+    <div
+      className={clsx(
+        !big &&
+          'relative w-full mx-auto sm:mt-8 sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-3xl 2xl:max-w-4xl sm:rounded-xl shadow-lg bg-white dark:bg-gray-800 p-6',
+        big &&
+          'relative container mx-auto sm:mt-8 lg:max-w-4xl xl:max-w-5xl 2xl:max-w-6xl sm:rounded-xl shadow-lg bg-white dark:bg-gray-800 p-6',
+      )}
+    >
       {children}
     </div>
   );
