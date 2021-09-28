6cc5827f9bde281e80853828b3bcda8d308bd4ee
Tom Kaeppler <tomkaeppler@web.de>
2021-09-28 11:53:35 +0200

    improve Button component.Add disabled state and improve transparent styling.

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -10,13 +10,14 @@ export function Button({ variant = 'primary', className, ...props }: Props) {
   return (
     <button
       className={clsx(
-        'items-center justify-center px-6 py-2 rounded font-medium hover:bg-opacity-90 ' +
+        'items-center justify-center px-6 py-2 rounded font-medium hover:bg-opacity-90 disabled:cursor-not-allowed' +
           (className ? ' ' + className : ''),
         {
           'bg-brand-500 text-white': variant === 'primary',
           'bg-gray-700 hover:bg-gray-600 text-gray-100': variant === 'secondary',
           'bg-red-500 text-white': variant === 'danger',
-          'bg-transparent text-gray-900 dark:text-white': variant === 'transparent',
+          'border-2 border-gray-700 bg-transparent text-gray-900 dark:text-white':
+            variant === 'transparent',
         },
       )}
       {...props}
