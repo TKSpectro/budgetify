0b6776ebc2351b6fabd7c5768710328d00cd5929
Tom Kaeppler <tomkaeppler@web.de>
2021-10-01 15:15:53 +0200

    need to force page naming with .page.tsx because all files in /pages are used. This needs to be done becuase of the new __generated__ files/folders in /pages

--- next.config.js
+++ next.config.js
@@ -57,6 +57,7 @@ const securityHeaders = [
  * @type {import('next').NextConfig}
  */
 module.exports = {
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
   webpack: (config) => {
     config.experiments = { topLevelAwait: true };
     //config.optimization.minimize = false;
