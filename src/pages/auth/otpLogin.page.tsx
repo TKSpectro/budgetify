1d184e62748436d40a646ae3b592a3b13574411a
Tom Käppler <tomkaeppler@web.de>
2021-10-04 16:13:18 +0200

    i18n otpLogin page.

--- src/pages/auth/otpLogin.page.tsx
+++ src/pages/auth/otpLogin.page.tsx
@@ -1,4 +1,7 @@
 import { gql, useMutation, useQuery } from '@apollo/client';
+import { GetServerSideProps } from 'next';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useRouter } from 'next/router';
 import React from 'react';
 import { useForm } from 'react-hook-form';
@@ -8,7 +11,6 @@ import { Error } from '~/components/UI/Error';
 import { Form } from '~/components/UI/Form';
 import { ME_QUERY } from '~/components/UI/Header';
 import { Input } from '~/components/UI/Input';
-import { Link } from '~/components/UI/Link';
 import { MeQuery, MeQueryVariables } from '~/components/UI/__generated__/Header.generated';
 import {
   OtpLoginMutation,
@@ -24,6 +26,8 @@ const LOGIN_MUTATION = gql`
 `;
 
 export default function OTPLogin() {
+  const { t } = useTranslation(['common', 'auth']);
+
   const { refetch } = useQuery<MeQuery, MeQueryVariables>(ME_QUERY);
   const router = useRouter();
 
@@ -51,32 +55,38 @@ export default function OTPLogin() {
   return (
     <Container>
       <div className="mb-2">
-        <Error title="Failed to login. Email or password is wrong!" error={error} />
+        <Error title={t('auth:loginError')} error={error} />
       </div>
 
       <Form form={loginForm} onSubmit={onSubmit}>
         <Input
-          label="Email"
+          label={t('email')}
           type="email"
           autoComplete="email"
           {...loginForm.register('email', {
-            required: { value: true, message: 'Email is required.' },
+            required: { value: true, message: t('emailMessage') },
           })}
         />
 
         <Input
-          label="One Time Password"
+          label={t('auth:otp')}
           type="password"
           autoComplete="one-time-code"
           {...loginForm.register('password', {
-            required: { value: true, message: 'One Time Password is required.' },
+            required: { value: true, message: t('auth:otpMessage') },
           })}
         />
 
-        <Button type="submit">Login</Button>
+        <Button type="submit">{t('login')}</Button>
       </Form>
-
-      <Link href="/auth/signup">No Account? No Problem! Signup</Link>
     </Container>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(ctx.locale || 'en', ['common', 'auth'])),
+    },
+  };
+};
