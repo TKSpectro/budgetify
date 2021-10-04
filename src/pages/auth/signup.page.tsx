93578bf4958b77e74a161b3b9802b2d645ec7d0f
Tom Käppler <tomkaeppler@web.de>
2021-10-04 15:28:04 +0200

    i18n signup page.

--- src/pages/auth/signup.page.tsx
+++ src/pages/auth/signup.page.tsx
@@ -1,4 +1,7 @@
 import { gql, useMutation, useQuery } from '@apollo/client';
+import { GetServerSideProps } from 'next';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useRouter } from 'next/dist/client/router';
 import React from 'react';
 import { useForm } from 'react-hook-form';
@@ -26,6 +29,8 @@ const SIGNUP_MUTATION = gql`
 `;
 
 export default function Signup() {
+  const { t } = useTranslation(['common', 'auth']);
+
   const { refetch } = useQuery<MeQuery, MeQueryVariables>(ME_QUERY);
   const router = useRouter();
 
@@ -52,53 +57,61 @@ export default function Signup() {
   return (
     <Container>
       <div className="mb-2">
-        <Error title="Failed to login. Email or password is wrong!" error={error} />
+        <Error title={t('auth:signupError')} error={error} />
       </div>
 
       <Form form={signupForm} onSubmit={onSubmit}>
         <Input
-          label="Firstname"
+          label={t('firstname')}
           type="text"
           autoComplete="given-name"
           {...signupForm.register('firstname', {
-            required: { value: true, message: 'Firstname is required.' },
-            maxLength: { value: 60, message: 'Firstname cant be longer than 60 characters.' },
+            required: { value: true, message: t('firstnameMessage') },
+            maxLength: { value: 60, message: t('firstnameLengthMessage') },
           })}
         />
 
         <Input
-          label="Lastname"
+          label={t('lastname')}
           type="text"
           autoComplete="family-name"
           {...signupForm.register('lastname', {
-            required: { value: true, message: 'Lastname is required.' },
-            maxLength: { value: 60, message: 'Lastname cant be longer than 60 characters.' },
+            required: { value: true, message: t('lastnameMessage') },
+            maxLength: { value: 60, message: t('lastnameLengthMessage') },
           })}
         />
 
         <Input
-          label="Email"
+          label={t('email')}
           type="email"
           autoComplete="email"
           {...signupForm.register('email', {
-            required: { value: true, message: 'Email is required.' },
+            required: { value: true, message: t('emailMessage') },
           })}
         />
 
         <Input
-          label="Password"
+          label={t('password')}
           type="password"
           autoComplete="new-password"
           {...signupForm.register('password', {
-            required: { value: true, message: 'Password is required.' },
-            minLength: { value: 6, message: 'Password must be at least 6 characters.' },
+            required: { value: true, message: t('passwordMessage') },
+            minLength: { value: 6, message: t('passwordLengthMessage') },
           })}
         />
 
-        <Button type="submit">Signup</Button>
+        <Button type="submit">{t('signup')}</Button>
       </Form>
 
-      <Link href="/auth/login">Do you already have an account? Login</Link>
+      <Link href="/auth/login">{t('auth:alreadyHaveAccountText')}</Link>
     </Container>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(ctx.locale || 'en', ['common', 'auth'])),
+    },
+  };
+};
