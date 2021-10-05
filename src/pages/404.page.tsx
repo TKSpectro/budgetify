ae966839eaaeff3196185cbdec27f057afc548e7
Tom Käppler <tomkaeppler@web.de>
2021-10-05 10:03:35 +0200

    i18n 404 page.

--- src/pages/404.page.tsx
+++ src/pages/404.page.tsx
@@ -1,7 +1,12 @@
+import { GetStaticProps } from 'next';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Head from 'next/head';
 import { Link } from '~/components/UI/Link';
 
 export default function Custom404() {
+  const { t } = useTranslation(['404', 'common']);
+
   return (
     <div>
       <Head>
@@ -12,10 +17,8 @@ export default function Custom404() {
           <div className="bg-gray-200 dark:bg-gray-800 shadow overflow-hidden sm:rounded-lg pb-8">
             <div className="text-center pt-8">
               <h1 className="text-9xl font-bold text-brand-500">404</h1>
-              <h2 className="text-6xl font-medium py-8">Page not found</h2>
-              <div className="text-2xl pb-8 px-12 font-medium">
-                The page you are looking for does not exist. It might have been moved or deleted.
-              </div>
+              <h2 className="text-6xl font-medium px-12 py-8">{t('title')}</h2>
+              <div className="text-2xl pb-8 px-12 font-medium">{t('description')}</div>
               <Link
                 href="/"
                 className="bg-brand-500 text-white font-semibold px-6 py-3 rounded-md mr-6"
@@ -29,3 +32,11 @@ export default function Custom404() {
     </div>
   );
 }
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(ctx.locale || 'en', ['404', 'common'])),
+    },
+  };
+};
