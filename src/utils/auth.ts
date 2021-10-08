85cfef2209fc9bbc12139a66e9aae7874cccbc61
Tom Kaeppler <tomkaeppler@web.de>
2021-10-08 14:07:48 +0200

    improve auth check jwt for issuedAt and expiresAt.

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,12 +1,11 @@
 import { AuthenticationError } from 'apollo-server-micro';
+import { compareAsc, compareDesc } from 'date-fns';
 import jwt from 'jsonwebtoken';
 import { GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
 import { prisma } from './prisma';
 
 type EncodedToken = {
   id: string;
-  email: string;
-  isEmail: boolean;
   iat: number;
   exp: number;
 };
@@ -16,10 +15,16 @@ export enum AuthRouteChecks {
   CHECK_HOUSEHOLD_OWNER = 'CHECK_HOUSEHOLD_OWNER',
   CHECK_GROUP_OWNER = 'CHECK_GROUP_OWNER',
 }
+/**
+ * This function can be called in the getServerSideProps function of a component
+ * to enforce that the user is logged in, else we will redirect him to either
+ * the login page or a given route
+ *
+ * @param context The context from getServerSideProps
+ * @param redirect The route to redirect if not authenticated (default: /api/login)
+ * @param checks Specific checks for authentication
+ */
 
-// This function can be called in the getServerSideProps function of a component
-// to enforce that the user is logged in, else we will redirect him to either
-// the login page or a given route
 export async function authenticatedRoute(
   context: GetServerSidePropsContext,
   redirect = '/auth/login',
@@ -28,13 +33,22 @@ export async function authenticatedRoute(
   try {
     // Check if the send authToken is a valid jwt.
     const data = jwt.verify(context.req.cookies.authToken, process.env.JWT_SECRET!) as EncodedToken;
-    if (!data.id) throw new AuthenticationError('Client sent a non valid jwt');
+
+    // jwt.iat and jwt.exp are in EPOCH Seconds, but we need EPOCH Milliseconds for JS Date -> * 1000
+    // Token issuedAt date has to be before current date
+    if (compareDesc(new Date(data.iat * 1000), new Date()) !== 1) {
+      throw new AuthenticationError('JWT is not valid.');
+    }
+    // Token expiresAt date hast to be after current date
+    if (compareAsc(new Date(data.exp * 1000), new Date()) !== 1) {
+      throw new AuthenticationError('JWT is not valid.');
+    }
 
     // Check if there is an actual user with the sent userId.
     const user = await prisma.user.findFirst({ where: { id: data.id } });
-    if (!user) throw new AuthenticationError('Client sent a non valid jwt');
+    if (!user) throw new AuthenticationError('JWT is not valid.');
 
-    let redirectCheckLocation: string = '';
+    let redirectCheckLocation = '';
     // If any checks were given, look for specific one and if they were found check them
     if (checks) {
       // Checks if the logged in user owns the requested household
@@ -59,7 +73,7 @@ export async function authenticatedRoute(
       }
     }
 
-    // If any check occured we can redirect to the set location
+    // If any check was set we can redirect to the set location
     if (redirectCheckLocation) {
       context.res.writeHead(302, {
         Location: redirectCheckLocation,
