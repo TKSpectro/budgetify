407f8ebd39a0d32e631346bc529f1a5baa8c586d
Tom Käppler <tomkaeppler@web.de>
2021-10-14 15:46:44 +0200

    small improvement to authRules.

--- src/graphql/authRules.ts
+++ src/graphql/authRules.ts
@@ -1,8 +1,7 @@
 import { prisma } from '~/utils/prisma';
 import { Context } from './context';
 
-export const authIsLoggedIn = (_: any, __: any, ctx: Context) => (ctx.user ? true : false);
-
+export const authIsLoggedIn = (_: any, __: any, ctx: Context) => !!ctx.user;
 export const authIsAdmin = (_: any, __: any, ctx: Context) => ctx.user.isAdmin;
 
 export const authIsGroupMember = async (_: any, args: any, ctx: Context) => {
