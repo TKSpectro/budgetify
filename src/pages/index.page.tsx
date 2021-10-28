90206413d9a585098cf6650f6dc0fb99f4318d05
Tom Kaeppler <tomkaeppler@web.de>
2021-10-28 08:27:07 +0200

    improve home page. show some marketing if the user is not logged in.

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -39,13 +39,12 @@ export const HOME_QUERY = gql`
   }
 `;
 
-// TODO: This page either needs to be a authenticatedRoute or handle a missing user -> Not logged in
-// status -> show some basic information about the product maybe
-
 export default function Home() {
   const { t } = useTranslation(['common', 'home']);
 
   const { data, loading, error } = useQuery<HomeQuery, HomeQueryVariables>(HOME_QUERY);
+
+  const loggedIn = !!data?.me;
 
   const households = data?.me?.households || [];
   const groups = data?.me?.groups || [];
@@ -56,59 +55,68 @@ export default function Home() {
         <title>budgetify</title>
       </Head>
 
-      <div className="grid grid-cols-1 xl:grid-cols-2 lg:gap-x-8 overflow-auto md:mx-16 md:px-4">
-        <Container>
-          <Error
-            title={t('home:householdsNotFoundError')}
-            error={households.length === 0 ? '' : undefined}
-          />
-          <Error title={t('loadingError')} error={error} />
-          <Loader loading={loading} />
+      {loggedIn ? (
+        <div className="grid grid-cols-1 xl:grid-cols-2 lg:gap-x-8 overflow-auto md:mx-16 md:px-4">
+          <Container>
+            <Error
+              title={t('home:householdsNotFoundError')}
+              error={households.length === 0 ? '' : undefined}
+            />
+            <Error title={t('loadingError')} error={error} />
+            <Loader loading={loading} />
 
-          {households.map((household) => {
-            return (
-              <Link href={`/households/${household?.id}`} passHref key={household?.id}>
-                <div className="border-2 border-gray-500 dark:bg-gray-800 dark:border-brand-500 p-3 mb-4 last:mb-0 rounded-lg hover:cursor-pointer">
-                  <div className="text-xl">
-                    {household?.name}
-                    <span className="float-right hidden sm:block">
-                      Balance: {household?.sumOfAllPayments}€
-                    </span>
+            {households.map((household) => {
+              return (
+                <Link href={`/households/${household?.id}`} passHref key={household?.id}>
+                  <div className="border-2 border-gray-500 dark:bg-gray-800 dark:border-brand-500 p-3 mb-4 last:mb-0 rounded-lg hover:cursor-pointer">
+                    <div className="text-xl">
+                      {household?.name}
+                      <span className="float-right hidden sm:block">
+                        Balance: {household?.sumOfAllPayments}€
+                      </span>
+                    </div>
+                    <div>
+                      {t('home:owner')}: {household?.owner?.name}
+                    </div>
+                    <span className="sm:hidden">Balance: {household?.sumOfAllPayments}€</span>
                   </div>
-                  <div>
-                    {t('home:owner')}: {household?.owner?.name}
-                  </div>
-                  <span className="sm:hidden">Balance: {household?.sumOfAllPayments}€</span>
-                </div>
-              </Link>
-            );
-          })}
-        </Container>
-        <Container>
-          <Error
-            title={t('home:groupsNotFoundError')}
-            error={groups.length === 0 ? '' : undefined}
-          />
-          <Loader loading={loading} />
+                </Link>
+              );
+            })}
+          </Container>
+          <Container>
+            <Error
+              title={t('home:groupsNotFoundError')}
+              error={groups.length === 0 ? '' : undefined}
+            />
+            <Loader loading={loading} />
 
-          {groups.map((group) => {
-            return (
-              <Link href={`/groups/${group?.id}`} passHref key={group?.id}>
-                <div className="border-2 border-gray-500 dark:bg-gray-800 dark:border-brand-500 p-3 mb-4 last:mb-0 rounded-lg hover:cursor-pointer">
-                  <div className="text-xl">
-                    {group?.name}
-                    <span className="float-right hidden sm:block">Balance: {group?.value}€</span>
+            {groups.map((group) => {
+              return (
+                <Link href={`/groups/${group?.id}`} passHref key={group?.id}>
+                  <div className="border-2 border-gray-500 dark:bg-gray-800 dark:border-brand-500 p-3 mb-4 last:mb-0 rounded-lg hover:cursor-pointer">
+                    <div className="text-xl">
+                      {group?.name}
+                      <span className="float-right hidden sm:block">Balance: {group?.value}€</span>
+                    </div>
+                    <div>
+                      {t('home:transactions')}: {group?.transactionCount}
+                    </div>
+                    <span className="sm:hidden">Balance: {group?.value}€</span>
                   </div>
-                  <div>
-                    {t('home:transactions')}: {group?.transactionCount}
-                  </div>
-                  <span className="sm:hidden">Balance: {group?.value}€</span>
-                </div>
-              </Link>
-            );
-          })}
-        </Container>
-      </div>
+                </Link>
+              );
+            })}
+          </Container>
+        </div>
+      ) : (
+        // TODO: Style index page with some marketing information etc.
+        // This gets shown if the user is not logged in
+        <div className="w-full text-center mt-16">
+          <div className="font-bold text-8xl text-brand-500">budgetify</div>
+          <div className="font-semibold text-2xl mt-4">Improve your household financials</div>
+        </div>
+      )}
     </div>
   );
 }
