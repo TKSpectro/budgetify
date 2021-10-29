c4827dec13c6c8ad627acf227ebacb1ef7c66dd2
Tom Kaeppler <tomkaeppler@web.de>
2021-10-29 08:12:47 +0200

    improve home page with some marketing information.

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,4 +1,10 @@
 import { gql, useQuery } from '@apollo/client';
+import {
+  AnnotationIcon,
+  GlobeAltIcon,
+  LightningBoltIcon,
+  ScaleIcon,
+} from '@heroicons/react/outline';
 import { GetServerSideProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Head from 'next/head';
@@ -48,6 +54,60 @@ export default function Home() {
 
   const households = data?.me?.households || [];
   const groups = data?.me?.groups || [];
+
+  const householdFeatures = [
+    {
+      name: 'Quick Overview',
+      description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+      icon: GlobeAltIcon,
+    },
+    {
+      name: 'Easy to use',
+      description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+      icon: ScaleIcon,
+    },
+    {
+      name: 'Graphs',
+      description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+      icon: LightningBoltIcon,
+    },
+    {
+      name: 'Easy Management',
+      description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+      icon: AnnotationIcon,
+    },
+  ];
+
+  const groupFeatures = [
+    {
+      name: 'Quick setup',
+      description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+      icon: LightningBoltIcon,
+    },
+    {
+      name: 'Invite system',
+      description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+      icon: GlobeAltIcon,
+    },
+    {
+      name: 'Transfers are instant',
+      description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+      icon: ScaleIcon,
+    },
+    {
+      name: 'Mobile notifications',
+      description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+      icon: AnnotationIcon,
+    },
+  ];
 
   return (
     <div>
@@ -110,12 +170,82 @@ export default function Home() {
           </Container>
         </div>
       ) : (
-        // TODO: Style index page with some marketing information etc.
-        // This gets shown if the user is not logged in
-        <div className="w-full text-center mt-16">
-          <div className="font-bold text-8xl text-brand-500">budgetify</div>
-          <div className="font-semibold text-2xl mt-4">Improve your household financials</div>
-        </div>
+        // TODO: Write Feature Stuff
+        <>
+          <div className="w-full text-center mt-16">
+            <div className="font-bold text-6xl lg:text-8xl text-brand-500">budgetify</div>
+            <div className="font-semibold text-2xl mt-8">Improve your financial management</div>
+          </div>
+          <div className="py-12">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              <div className="lg:text-center">
+                <h2 className="text-base text-brand-500 font-semibold tracking-wide uppercase">
+                  Households
+                </h2>
+                <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                  Managing money made easy
+                </p>
+                <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+                  Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum
+                  cupiditate veritatis in accusamus quisquam.
+                </p>
+              </div>
+
+              <div className="mt-10">
+                <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+                  {householdFeatures.map((feature) => (
+                    <div key={feature.name} className="relative">
+                      <dt>
+                        <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-brand-500 text-white">
+                          <feature.icon className="h-6 w-6" aria-hidden="true" />
+                        </div>
+                        <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
+                          {feature.name}
+                        </p>
+                      </dt>
+                      <dd className="mt-2 ml-16 text-base text-gray-500">{feature.description}</dd>
+                    </div>
+                  ))}
+                </dl>
+              </div>
+            </div>
+          </div>
+
+          <div className="py-12">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              <div className="lg:text-center">
+                <h2 className="text-base text-brand-500 font-semibold tracking-wide uppercase">
+                  Groups
+                </h2>
+                <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                  Moneypools for all your vacations and group activities
+                </p>
+                <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+                  Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum
+                  cupiditate veritatis in accusamus quisquam.
+                </p>
+              </div>
+
+              <div className="mt-10">
+                <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+                  {groupFeatures.map((feature) => (
+                    <div key={feature.name} className="relative">
+                      <dt>
+                        <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-brand-500 text-white">
+                          <feature.icon className="h-6 w-6" aria-hidden="true" />
+                        </div>
+                        <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
+                          {feature.name}
+                        </p>
+                      </dt>
+                      <dd className="mt-2 ml-16 text-base text-gray-500">{feature.description}</dd>
+                    </div>
+                  ))}
+                </dl>
+              </div>
+            </div>
+          </div>
+        </>
       )}
     </div>
   );
