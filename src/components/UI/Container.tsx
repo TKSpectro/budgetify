08aa8d90dbaa99a85a9b81e1dfc2a2aea29fdaac
Tom Kaeppler <tomkaeppler@web.de>
2021-11-11 16:02:59 +0100

    improve Container component.

--- src/components/UI/Container.tsx
+++ src/components/UI/Container.tsx
@@ -12,13 +12,13 @@ interface Props {
  */
 export function Container({ title, action, children }: Props) {
   return (
-    <div className="relative w-full mx-auto mt-8 mb-4 sm:mt-8 p-6 sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-3xl 2xl:max-w-4xl sm:rounded-xl shadow-lg bg-white dark:bg-gray-800 ">
+    <div className="relative w-full mx-auto mb-4 sm:mt-8 p-6 sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-3xl 2xl:max-w-4xl sm:rounded-xl shadow-lg bg-white dark:bg-gray-800 ">
       {(title || action) && (
-        <div className="flex items-center justify-between mb-4">
+        <div className="lg:flex items-center justify-between mb-4">
           {title && (
             <h1 className="text-3xl font-bold text-gray-900 dark:text-gray-100">{title}</h1>
           )}
-          {action}
+          <div className="mt-4 lg:mt-0">{action}</div>
         </div>
       )}
       {children}
