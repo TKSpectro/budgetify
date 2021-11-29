0271a9d222bb081b1b568df388b391d9c23bb63c
Tom Kaeppler <tomkaeppler@web.de>
2021-11-29 13:36:52 +0100

    improve Input component. Can add a prepended icon now.

--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -1,27 +1,44 @@
-import { ComponentProps, forwardRef } from 'react';
+import clsx from 'clsx';
+import { ComponentProps, forwardRef, ReactNode } from 'react';
 import { InputError } from './Form';
 
 interface Props extends ComponentProps<'input'> {
   label: string;
   className?: string;
+  icon?: ReactNode;
 }
 
 /**
  * Custom Input component. Should be used inside the custom Form component
  */
 export const Input = forwardRef<HTMLInputElement, Props>(function Input(
-  { label, type = 'text', className, ...props },
+  { label, type = 'text', className, icon, ...props },
   ref,
 ) {
   return (
     <label className={className}>
       <div className="py-1 px-2 text-md font-semibold ">{label}</div>
-      <input
-        className="bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-200 w-full rounded-md px-4 py-2 border focus:border-brand-500 focus:ring-brand-500"
-        type={type}
-        ref={ref}
-        {...props}
-      />
+
+      <div className="relative">
+        {icon && (
+          <div className="absolute inset-y-0 pl-2 left-0 flex items-center pointer-events-none">
+            {icon}
+
+            <div className="ml-1.5 w-[1px] h-full bg-gray-800"></div>
+          </div>
+        )}
+
+        <input
+          className={clsx(
+            'bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-200 w-full rounded-md py-2 border focus:border-brand-500 focus:ring-brand-500',
+            icon && 'pl-12 pr-4 ',
+            !icon && 'px-4',
+          )}
+          type={type}
+          ref={ref}
+          {...props}
+        />
+      </div>
 
       <InputError name={props.name} />
     </label>
