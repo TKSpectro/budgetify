aaa5f500dbbe0f2f2199aca29252c9cd87e10801
Tom Kaeppler <tomkaeppler@web.de>
2021-11-30 09:17:22 +0100

    improve Container, center title if no action is passed.

--- src/components/UI/Container.tsx
+++ src/components/UI/Container.tsx
@@ -16,12 +16,13 @@ export function Container({ title, titleClassName, action, children }: Props) {
   return (
     <div className="relative w-full mx-auto mb-4 sm:mt-8 p-6 sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-3xl 2xl:max-w-4xl sm:rounded-xl shadow-lg bg-white dark:bg-gray-800 ">
       {(title || action) && (
-        <div className="flex flex-wrap items-center justify-between mb-4">
+        <div className={clsx('mb-4', action && 'flex flex-wrap items-center justify-between')}>
           {title && (
             <h1
               className={clsx(
                 titleClassName,
-                !titleClassName && 'text-3xl font-bold text-gray-900 dark:text-gray-100',
+                !titleClassName &&
+                  'text-3xl font-bold text-center text-gray-900 dark:text-gray-100',
               )}
             >
               {title}
